9652e8cb315a8f4ef539ef90ae2023b8ef04aa56
Braydn <tcrbraydn@gmail.com>
2023-12-06 14:14:16 -0700

    init scripts

---
+++ init/mac.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env zsh
+###################################################################
+#Name          mac.sh
+#Description   Installs all dependencies on new Mac
+#Author        Braydn Tanner
+###################################################################
+
+set -e
+
+install_brew_packages () {
+#  brew bundle --file ./Brewfile
+  # go to plugins custom and git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+  # plugins+=(zsh-nvm)in .zshrc
+}
+
+setup_zsh () {
+  bash ./zsh.sh
+}
+
+symlink () {
+
+}
+
+main () {
+  install_brew_packages
+  setup_zsh
+  symlink
+}
+
+main $@
---
+++ init/zsh.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env zsh
+if [ -d ~/.oh-my-zsh ]; then
+	echo "oh-my-zsh is installed"
+else
+	echo "oh-my-zsh is not installed. Installing..."
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "oh-my-zsh is installed"
+fi
+
+cd ~/.oh-my-zsh/custom
+echo "alias ls='exa -a'
+alias cat='bat'
+alias grep='rg'
+alias find='fd'
+alias ipw='ipconfig getifaddr en0'
+alias ipe='ipconfig getifaddr en1'
+" > aliases.zsh
+
+ln -sf ~/.odyssey/alacritty ~/.config/alacritty
+ln -sf ~/.odyssey/nvim ~/.config/nvim
+ln -sf ~/.odyssey/ssh ~/.ssh
+ln -sf ~/.odyssey/tmux/tmux.conf ~/.tmux.conf
+
